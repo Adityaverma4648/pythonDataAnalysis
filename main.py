a558de8bccd96b20f257ff5afde773f317a02882
Aditya verma <87409406+buttkicker3124@users.noreply.github.com>
2022-09-23 11:36:07 +0530

    created copy car_data2 - to store clean data

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os;
 
 df = pd.read_csv("./Toyota.csv")
 car_data1 = pd.DataFrame(df)
-missing = pd.DataFrame(df)
+# missing = pd.DataFrame(car_data1.row.isnull())
 
 # whole data------------
 print(car_data1)
@@ -32,10 +32,32 @@ print('dtypes : ')
 #  gives datatypes of all columns
 print(car_data1.dtypes)
 
+# info and describe 
+# describe returns the stastical data of the csv read by the sys.
+
+print('car info : ')
+print(car_data1.info)
+
 # accessing columns seperately
 
 print('Accesing column name age : ')
 print(car_data1.Age)
 
-print('car info : ')
-print(car_data1.info)
+# Cleaning the data
+
+car_data2 = pd.DataFrame(df)
+
+# print(car_data2.Age.unique());
+
+print(car_data2.MetColor.unique())
+
+car_data2 =  car_data2.MetColor.replace(1.,1)
+car_data2 = car_data2.Metcolor.replace(0.,0)
+
+print(car_data2.MetColor.unique())
+
+#car_data2 =  car_data2.Age.astype('int64')
+#car_data2 =  car_data2.MetColor.astype('object')
+#car_data2 =  car_data2.Automatic.astype('object')
+#car_data2 =  car_data2.Doors.astype('int64')
+
